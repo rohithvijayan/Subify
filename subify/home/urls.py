758876670b9f42525000bba0da2a14b0495659b3
rohithvijayan <rohithvij20@gmail.com>
2024-06-19 23:40:11 +0530

    Added Feature to generate  SUbtitles for user uploaded videos

--- subify/home/urls.py
+++ subify/home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path("",views.home,name="home"),
-    path("upload",views.vidUpload,name="upload")
+    path("upload",views.vidUpload,name="upload"),
+    path("generate/<int:id>/",views.gen_sub,name="generate")
 ]
--- subify/home/views.py
+++ subify/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect,HttpResponse
 from .models import upload_video
+import openai
+from moviepy.editor import *
+import srt
+from datetime import timedelta
+import os
+from openai.types.audio import transcription,translation,Transcription
 # Create your views here.
 def home(request):
     return render(request,"home/index.html")
@@ -11,6 +17,36 @@ def vidUpload(request):
         videofile=videos["vid"]
         new_vid=upload_video(title=title,video=videofile)
         new_vid.save()
-        return HttpResponse("SUCCESS") 
+        
+        return redirect("generate",id=new_vid.id)
        
     return render(request,"home/upload.html")
+
+def gen_sub(request,id):
+    source_vid=upload_video.objects.get(id=id)
+    sub_name=source_vid.title
+    file_path=source_vid.video.path
+    audioclip=AudioFileClip(file_path)
+    audioclip.write_audiofile(f"media/converted_audio/{sub_name}.wav")
+    whisper=openai.OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    audio=open(file=f"media/converted_audio/{sub_name}.wav",mode='rb')
+    sub=whisper.audio.transcriptions.create(file=audio,model="whisper-1",response_format='verbose_json',temperature=0,timestamp_granularities=['segment'])
+    print(sub)
+    segments=sub.segments
+    content_list=[]
+    for item in segments:
+        index=item['id']
+        start=item['start']
+        end=item['end']
+        text=item['text']
+        subtitle=srt.Subtitle(index=index,start=timedelta(seconds=start),end=timedelta(seconds=end),content=text)
+        content_list.append(subtitle)
+        
+    with open(f"media/subtitles/{sub_name}.srt","w+") as srtfile:
+        srtfile.write(srt.compose(subtitles=content_list))
+        srtfile.close()
+    return HttpResponse(f"Subtitle generated successfully for {sub_name}.srt")
+
+    
+    
+        
