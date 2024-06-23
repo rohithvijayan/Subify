84653f3f82c3d31c080d4adafef2ce6fac035958
rohithvijayan <106431458+rohithvijayan@users.noreply.github.com>
2024-06-23 21:38:16 +0530

    Update settings.py

--- subify/subify/settings.py
+++ subify/subify/settings.py
@@ -89,13 +89,15 @@ DATABASES = {
 }
 '''
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+
+if not DEBUG:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('postgresql://subify:dySiWTu90GNRKgjzu785v8W5LMDHjJxb@dpg-cps4fst6l47c73dvo0tg-a/subifydb'))}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'db.sqlite3',
+        }}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
